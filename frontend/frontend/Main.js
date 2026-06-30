01839f9ee938c4cbf57ed7b44d1a58b28440cec1
TeodoraPadishanska <teodorapadisanska@gmail.com>
2026-06-30 11:00:21 +0300

    js

--- frontend/frontend/Main.js
+++ frontend/frontend/Main.js
@@ -1,8 +1,9 @@
-let products = [];
 fetch("http://localhost:8080/business/products")
 .then(response => response.json())
 .then(products => {
     let container = document.getElementById('promo-cards');
+    container.innerHTML = ``;
+
     products.forEach(product => {
         const card = createProductCard(product);
         container.append(card);
@@ -11,7 +12,10 @@ fetch("http://localhost:8080/business/products")
 .catch(error => console.log(error));
 
 function calculateDiscount(product) {
-    if(product.isOnSale){
+    if (product.isOnSale == null){
+        product.isOnSale = false;
+    }
+    if(product.isOnSale || product.price > 0){
         return Math.round(((product.price - product.salePrice) / product.price) * 100);
     }
     return 0;
@@ -25,23 +29,19 @@ function createProductCard(product) {
     card.innerHTML = `
         ${product.isOnSale ? `<span class = "discount">- ${discount}%</span>` : ""}
         <button class="favourite-btn">♡</button>
-        <img class="card-img-top" src="${product.imageUrl}" alt="${product.name}">
+        <img class="card-img-top" src="${product.imgUrl}" alt="${product.name}">
         <div class="card-body">
             <h5 class="card-title">${product.name}</h5>
         </div>
         <div class="price-container">
             <div class="price-row">
-        <span class="new-price">
-          ${product.isOnSale ? product.salePrice : product.price} €
-        </span>
+                <span class="new-price">
+                    ${product.isOnSale ? product.salePrice : product.price}&nbsp;€
+                </span>
+                ${product.isOnSale ? `<s class="old-price">${product.price}&nbsp;€</s>` : ""}
+            </div>
+        </div>
 
-        ${product.isOnSale ? `<s class="old-price">${product.price} €</s>` : ""}
-      </div>
-    </div>
-
-    <button class="add_item btn btn-outline-dark" style="margin: 12px">
-      Добави
-    </button>
-    `;
+    <button class="add_item btn btn-outline-dark" style="margin: 12px">Добави</button>`;
     return card;
 }
